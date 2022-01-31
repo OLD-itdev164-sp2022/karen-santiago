abe8015258e17dc285bc6120bfc60d1189c94cb0
santiak3 <71164211+santiak3@users.noreply.github.com>
2022-01-30 23:40:06 -0600

    Completed Activity 1

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -1,8 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Blog`,
+    description: `ITDEV-164 Gatsby Blog`,
+    author: `Karen Santiago`,
+    contact: {
+        name: `Karen Santiago`,
+        company: `Blog Inc.`,
+        address: `PO Box 107`,
+    },
     siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
   },
   plugins: [
