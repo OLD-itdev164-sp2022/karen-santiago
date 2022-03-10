7c82fa0104d079150ea7d2e1e5ddc3b7449798a8
santiak3 <71164211+santiak3@users.noreply.github.com>
2022-03-09 20:25:59 -0600

    Completed Activity 5

--- blog/gatsby-config.js
+++ blog/gatsby-config.js
@@ -3,7 +3,7 @@ require ('dotenv').config({
 });
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Blog`,
+    title: `Random Thought`,
     description: `ITDEV-164 Gatsby Blog`,
     author: `Karen Santiago`,
     contact: {
