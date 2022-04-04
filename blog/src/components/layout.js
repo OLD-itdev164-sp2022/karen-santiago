ba17285f7e045b9509be14bd8422b511088d0ec0
santiak3 <71164211+santiak3@users.noreply.github.com>
2022-04-04 14:48:25 -0500

    Replaced main and footer with corresponding  semantic components

--- blog/src/components/layout.js
+++ blog/src/components/layout.js
@@ -9,10 +9,11 @@ import * as React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
 import styled, { ThemeProvider } from 'styled-components'
-
 import { Gray } from '../themes/Gray'
 import { Header } from "./Header"
 import GlobalStyle from './GlobalStyle'
+import { Main } from './Main'
+import { Footer } from './Footer'
 
 const Content = styled.div`
   margin: 0 auto;
@@ -37,15 +38,12 @@ const Layout = ({ children }) => {
     <GlobalStyle />
       <Header siteTitle={data.site.siteMetadata.title || `Title`} />
       <Content>
-        <main>{children}</main>
-        <footer
-        style ={{
-          marginTop: `2rem`,
-        }}>
+        <Main>{children}</Main>
+        <Footer>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.com">Gatsby</a>
-        </footer>
+        </Footer>
       </Content>
     </ThemeProvider>
   )
