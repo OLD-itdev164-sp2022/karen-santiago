a15defbaee41a0311e799ec5445312940376aeb8
santiak3 <71164211+santiak3@users.noreply.github.com>
2022-04-06 20:30:01 -0500

    Completed Activity 8

--- blog/src/components/Header/Header.js
+++ blog/src/components/Header/Header.js
@@ -9,7 +9,7 @@ import { IconButton } from '../Button'
 import { Section } from '../Section'
 
 const Outer = styled.header`
-  background: ${props => props.theme.header.backgroundColor};
+  background: ${({ theme }) => theme.variants.header.primary.backgroundColor};
   margin-bottom: 1.45rem;
 `
 
@@ -39,7 +39,7 @@ const Header = ({ siteTitle }) => (
             </H1>
           </Section>
             <Section width={1/12}>
-              <IconButton icon={<Search />} />
+              <IconButton icon={<Search />} variant="contrast"/>
             </Section>
         </Section>
     </Inner>
